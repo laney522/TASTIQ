a5f0e6e49fe235cc9d1b90abb8e84bb39b8eaf79
laney522 <kassandrajang@Mac.attlocal.net>
2025-08-13 12:54:44 -0400

    db model created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js'; 
 
 const app = express();
 
+connectDB();
 
-app.listen(5001, () => console.log("Server is running on port 5001"));
+app.get("/", (req, res) => res.send("Hello from the server"));
+
+app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
